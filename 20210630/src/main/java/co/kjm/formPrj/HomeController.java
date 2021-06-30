41a5ec102a8ab7d73934fdf5fbe8bde52f73a99a
admin <admin@YD02-14>
2021-06-30 09:16:17 +0900

    Spring-form

---
+++ 20210630/src/main/java/co/kjm/formPrj/HomeController.java
@@ -0,0 +1,16 @@
+package co.kjm.formPrj;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+	public String home(Model model) {
+		
+		return "home";
+	}
+	
+}
---
+++ 20210630/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Spring Form</title>
+</head>
+<body>
+	<div align="center">
+		<div>
+			<h1>Home</h1>
+		</div>
+	</div>
+</body>
+</html>
